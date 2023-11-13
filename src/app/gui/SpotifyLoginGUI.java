d72ca0dabd82a8d6a500c19adaa76e3e1c74347a
aryanhakim17 <aryan.hakim@mail.utoronto.ca>
2023-11-13 14:26:24 -0500

    Updated the GUI for the Login page.

--- src/app/gui/SpotifyLoginGUI.java
+++ src/app/gui/SpotifyLoginGUI.java
@@ -5,21 +5,28 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class LoginGUI extends JFrame {
+public class SpotifyLoginGUI extends JFrame {
 
     private JTextField usernameField;
     private JPasswordField passwordField;
 
-    public LoginGUI() {
+    public SpotifyLoginGUI() {
         // Set up the frame
-        setTitle("Login Page");
-        setSize(400, 400);
+        setTitle("Spotify Login");
+        setSize(400, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        setResizable(false);
+
+        // Create main panel with Spotify green background
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new GridBagLayout());
+        mainPanel.setBackground(new Color(30, 215, 96)); // Spotify Green
 
         // Create components
-        JLabel title = new JLabel("Login");
-        title.setFont(new Font("Arial", Font.BOLD, 20));
+        JLabel title = new JLabel("Spotify Login");
+        title.setFont(new Font("Arial", Font.BOLD, 24));
+        title.setForeground(Color.BLACK);
 
         JLabel usernameLabel = new JLabel("Username:");
         JLabel passwordLabel = new JLabel("Password:");
@@ -28,38 +35,40 @@ public class LoginGUI extends JFrame {
         passwordField = new JPasswordField(20);
 
         JButton loginButton = new JButton("Login");
-        loginButton.setBackground(new Color(30, 144, 255)); // Dodger Blue
-        loginButton.setForeground(Color.WHITE);
+        loginButton.setBackground(new Color(30, 215, 96)); // Spotify Green
+        loginButton.setForeground(Color.BLACK);
         loginButton.setFocusPainted(false);
 
         // Set up layout
-        setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 5, 5, 5);
 
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 2;
-        add(title, gbc);
+        mainPanel.add(title, gbc);
 
         gbc.gridwidth = 1;
         gbc.gridy = 1;
-        add(usernameLabel, gbc);
+        mainPanel.add(usernameLabel, gbc);
 
         gbc.gridx = 1;
-        add(usernameField, gbc);
+        mainPanel.add(usernameField, gbc);
 
         gbc.gridx = 0;
         gbc.gridy = 2;
-        add(passwordLabel, gbc);
+        mainPanel.add(passwordLabel, gbc);
 
         gbc.gridx = 1;
-        add(passwordField, gbc);
+        mainPanel.add(passwordField, gbc);
 
         gbc.gridx = 0;
         gbc.gridy = 3;
         gbc.gridwidth = 2;
-        add(loginButton, gbc);
+        mainPanel.add(loginButton, gbc);
+
+        // Set the main panel as the content pane of the JFrame
+        setContentPane(mainPanel);
 
         // Add action listener to the login button
         loginButton.addActionListener(new ActionListener() {
@@ -71,9 +80,9 @@ public class LoginGUI extends JFrame {
 
                 // Validate username and password
                 if ("yourUsername".equals(username) && "yourPassword".equals(new String(password))) {
-                    JOptionPane.showMessageDialog(LoginGUI.this, "Login successful!");
+                    JOptionPane.showMessageDialog(SpotifyLoginGUI.this, "Login successful!");
                 } else {
-                    JOptionPane.showMessageDialog(LoginGUI.this, "Invalid username or password. Try again.");
+                    JOptionPane.showMessageDialog(SpotifyLoginGUI.this, "Invalid username or password. Try again.");
                 }
 
                 // Clear fields after login attempt
@@ -87,7 +96,7 @@ public class LoginGUI extends JFrame {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new LoginGUI().setVisible(true);
+                new SpotifyLoginGUI().setVisible(true);
             }
         });
     }
