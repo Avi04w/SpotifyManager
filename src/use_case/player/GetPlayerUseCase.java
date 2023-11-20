7370b0041f378b070631fcdbe98a0d795949c25e
Avi Walia <78369627+Avi04w@users.noreply.github.com>
2023-11-20 01:24:46 -0500

    Added use case for getting player object.
    Signed-off-by: Avi Walia <78369627+Avi04w@users.noreply.github.com>

--- src/use_case/player/GetPlayerUseCase.java
+++ src/use_case/player/GetPlayerUseCase.java
@@ -1,4 +1,17 @@
 package use_case.player;
 
+import api.PlayerDB;
+import api.Authorization;
+import entity.Player;
+
 public class GetPlayerUseCase {
+    private final PlayerDB playerDB;
+
+    public GetPlayerUseCase(PlayerDB playerDB) {
+        this.playerDB = playerDB;
+    }
+
+    public Player getPlayer(Authorization authorization) {
+        return playerDB.getPlayer(authorization);
+    }
 }
