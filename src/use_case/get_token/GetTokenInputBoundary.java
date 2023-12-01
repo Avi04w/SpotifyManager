6b9fa5178aa6efa73dec65eaef59f762d3020717
SDG1804 <s.diazgonzalez@mail.utoronto.ca>
2023-12-01 14:19:24 -0500

    Deleted more unused files for the GetToken use case.

--- src/use_case/get_token/GetTokenInputBoundary.java
+++
@@ -1,8 +0,0 @@
-package use_case.get_token;
-import data_access.Authorization;
-import data_access.Token;
-
-public interface GetTokenInputBoundary {
-
-    void execute(GetTokenInputData getTokenInputData, Authorization auth);
-}
